4f6bbff4b127f009843cc25ea7cca5da1cb3ea4c
Scott Cote <scottccote@gmail.com>
2024-05-22 11:51:35 -0500

    disabled app runner as being launched vi startup by removing component annotation and switched servlet container to jetty

--- src/main/java/com/coteware/springscroll/example/component/AppRunner.java
+++ src/main/java/com/coteware/springscroll/example/component/AppRunner.java
@@ -5,11 +5,11 @@ import com.coteware.springscroll.example.service.GitHubLookupService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Component;
 
 import java.util.concurrent.CompletableFuture;
 
-@Component
+//@Component
 public class AppRunner implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(AppRunner.class);
